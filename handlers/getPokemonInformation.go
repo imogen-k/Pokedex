eb528367df8b3220ace732268d54d82e44a0d4d9
Imogen Kraak <imogen.kraak@aluma.io>
2022-01-13 16:52:24 +0000

    Implement and test getPokemonInformation endpoint

--- handlers/getPokemonInformation.go
+++ handlers/getPokemonInformation.go
@@ -1,7 +1,11 @@
 package handlers
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 type GetPokemonInformationHandler struct {
@@ -11,6 +15,124 @@ func NewGetPokemonInformationHandler() *GetPokemonInformationHandler {
 	return &GetPokemonInformationHandler{}
 }
 
+type PokemonSpecies struct {
+	Name    string `json:"name"`
+	Species struct {
+		URL string `json:"url"`
+	}
+}
+
+type PokemonInformation struct {
+	FlavorTextEntries []struct {
+		FlavorText string `json:"flavor_text"`
+		Language   struct {
+			Name string `json:"name"`
+		} `json:"language"`
+	} `json:"flavor_text_entries"`
+	Habitat struct {
+		Name string `json:"name"`
+	} `json:"habitat"`
+	IsLegendary bool   `json:"is_legendary"`
+	Name        string `json:"name"`
+}
+
+type GetPokemonInformationResponse struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Habitat     string `json:"habitat"`
+	IsLegendary bool   `json:"is_legendary"`
+}
+
 // Handle GET /pokemon/{name}
 func (p *GetPokemonInformationHandler) Handle(w http.ResponseWriter, r *http.Request) {
+	name := strings.TrimPrefix(r.URL.Path, "/pokemon/")
+
+	// find pokemon species URL with pokemon name
+	url, err := GetSpeciesUrlFromName(name)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	// use url to call API to find habitat, description and legendary status
+	pokemonInformation, err := GetPokemonInformationFromUrl(url)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// get english description from flavor text entries
+	englishDescription := GetEnglishDescription(pokemonInformation)
+
+	getPokemonInformationResponse := &GetPokemonInformationResponse{
+		Name:        name,
+		Description: englishDescription,
+		Habitat:     pokemonInformation.Habitat.Name,
+		IsLegendary: pokemonInformation.IsLegendary,
+	}
+
+	jsonResponse, err := json.Marshal(getPokemonInformationResponse)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonResponse)
+}
+
+func GetSpeciesUrlFromName(name string) (string, error) {
+	response, err := http.Get(fmt.Sprintf("https://pokeapi.co/api/v2/pokemon/%s", name))
+	if err != nil {
+		return "", err
+	}
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return "", err
+	}
+
+	defer response.Body.Close()
+
+	var urlResponseObject PokemonSpecies
+	err = json.Unmarshal(responseData, &urlResponseObject)
+	if err != nil {
+		return "", err
+	}
+
+	return urlResponseObject.Species.URL, nil
+}
+
+func GetPokemonInformationFromUrl(url string) (PokemonInformation, error) {
+	response, err := http.Get(url)
+	if err != nil {
+		return PokemonInformation{}, err
+	}
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return PokemonInformation{}, err
+	}
+
+	defer response.Body.Close()
+
+	var informationResponseObject PokemonInformation
+	err = json.Unmarshal(responseData, &informationResponseObject)
+	if err != nil {
+		return PokemonInformation{}, err
+	}
+
+	return informationResponseObject, nil
+}
+
+func GetEnglishDescription(information PokemonInformation) string {
+	var englishDescription string
+	for _, description := range information.FlavorTextEntries {
+		if description.Language.Name == "en" {
+			englishDescription = description.FlavorText
+			englishDescription = strings.Replace(englishDescription, "\n", " ", -1)
+
+		}
+	}
+	return englishDescription
 }
--- handlers/getPokemonInformation_test.go
+++ handlers/getPokemonInformation_test.go
@@ -1,1 +1,82 @@
 package handlers_test
+
+import (
+	"Pokedex/handlers"
+	"encoding/json"
+	"github.com/stretchr/testify/suite"
+	"io/ioutil"
+	http2 "net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type GetPokemonInformationTestSuite struct {
+	suite.Suite
+	sut              *handlers.GetPokemonInformationHandler
+	responseRecorder *httptest.ResponseRecorder
+	requestBuilder   *http2.Request
+}
+
+func (suite *GetPokemonInformationTestSuite) SetupTest() {
+	suite.sut = handlers.NewGetPokemonInformationHandler()
+	suite.requestBuilder = httptest.NewRequest(http2.MethodGet, "/pokemon/metapod", nil)
+	suite.responseRecorder = httptest.NewRecorder()
+}
+
+func TestGetPokemonInformationTestSuiteRunner(t *testing.T) {
+	suite.Run(t, new(GetPokemonInformationTestSuite))
+}
+
+func (suite *GetPokemonInformationTestSuite) TestReturns_StatusOk_On_Success_With_Correct_Response_Values() {
+	expectedName := "metapod"
+	expectedDescription := "Even though it is encased in a sturdy shell, the body inside is tender. It can’t withstand a harsh attack."
+	expectedHabitat := "forest"
+	expectedLegendaryStatus := false
+
+	request := suite.requestBuilder
+
+	suite.sut.Handle(suite.responseRecorder, request)
+	suite.Assert().Equal(http2.StatusOK, suite.responseRecorder.Code)
+
+	body, err := ioutil.ReadAll(suite.responseRecorder.Body)
+	suite.Assert().Nil(err)
+
+	var response handlers.GetPokemonInformationResponse
+	err = json.Unmarshal(body, &response)
+	suite.Assert().Nil(err)
+
+	suite.Assert().Equal(expectedName, response.Name)
+	suite.Assert().Equal(expectedDescription, response.Description)
+	suite.Assert().Equal(expectedHabitat, response.Habitat)
+	suite.Assert().Equal(expectedLegendaryStatus, response.IsLegendary)
+}
+
+func (suite *GetPokemonInformationTestSuite) TestReturns_InternalServerErr_If_No_Name_Supplied_In_RequestUrl() {
+	request := httptest.NewRequest(http2.MethodGet, "/pokemon/", nil)
+
+	suite.sut.Handle(suite.responseRecorder, request)
+
+	suite.Assert().Equal(http2.StatusInternalServerError, suite.responseRecorder.Code)
+}
+
+func (suite *GetPokemonInformationTestSuite) TestReturns_StatusNotFound_If_Pokemon_Does_Not_Exist() {
+	request := httptest.NewRequest(http2.MethodGet, "/pokemon/fakepokemon", nil)
+
+	suite.sut.Handle(suite.responseRecorder, request)
+
+	suite.Assert().Equal(http2.StatusNotFound, suite.responseRecorder.Code)
+}
+
+func (suite *GetPokemonInformationTestSuite) TestReturns_Error_If_Decoding_Fails() {
+	request := suite.requestBuilder
+
+	suite.sut.Handle(suite.responseRecorder, request)
+
+	suite.Assert().Equal(http2.StatusOK, suite.responseRecorder.Code)
+
+	body := []byte(`{"name":bad_value}`)
+
+	var response handlers.GetPokemonInformationResponse
+	err := json.Unmarshal(body, &response)
+	suite.Assert().NotNil(err)
+}
