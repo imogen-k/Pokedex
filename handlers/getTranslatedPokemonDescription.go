2b8311afd00e55f478eb1a9769e74ba70da93ff4
Imogen Kraak <imogen.kraak@aluma.io>
2022-01-13 17:25:49 +0000

    Implement and test getTranslatedPokemonInformation endpoint

--- handlers/getTranslatedPokemonDescription.go
+++ handlers/getTranslatedPokemonDescription.go
@@ -1,7 +1,11 @@
 package handlers
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
+	"net/url"
+	"strings"
 )
 
 type GetTranslatedPokemonDescriptionHandler struct {
@@ -11,6 +15,117 @@ func NewGetTranslatedPokemonDescriptionHandler() *GetTranslatedPokemonDescriptio
 	return &GetTranslatedPokemonDescriptionHandler{}
 }
 
+type TranslatedDescriptionResponse struct {
+	Success struct {
+		Total int `json:"total"`
+	} `json:"success"`
+	Contents struct {
+		Translated string `json:"translated"`
+	} `json:"contents"`
+}
+
 // Handle GET /pokemon/translated/{name}
 func (p *GetTranslatedPokemonDescriptionHandler) Handle(w http.ResponseWriter, r *http.Request) {
+	name := strings.TrimPrefix(r.URL.Path, "/pokemon/translated/")
+
+	// find pokemon species URL with pokemon name
+	url, err := GetSpeciesUrlFromName(name)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	// use url to call API to find habitat, description and legendary status
+	pokemonInformation, err := GetPokemonInformationFromUrl(url)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	englishDescription := GetEnglishDescription(pokemonInformation)
+
+	// if pokemon habitat is cave, or if it's a legendary pokemon, use yoda translation
+	// if not, use shakespeare translation
+	translatedDescription, err := getTranslatedDescription(pokemonInformation, englishDescription)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	getPokemonInformationResponse := &GetPokemonInformationResponse{
+		Name:        name,
+		Description: translatedDescription,
+		Habitat:     pokemonInformation.Habitat.Name,
+		IsLegendary: pokemonInformation.IsLegendary,
+	}
+
+	jsonResponse, err := json.Marshal(getPokemonInformationResponse)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonResponse)
+}
+
+func getTranslatedDescription(pokemonInformation PokemonInformation, description string) (string, error) {
+	var translatedDescription string
+
+	params := url.Values{}
+	params.Add("text", description)
+
+	if pokemonInformation.Habitat.Name == "cave" || pokemonInformation.IsLegendary == true {
+		response, err := http.PostForm("https://api.funtranslations.com/translate/yoda.json", params)
+		if err != nil {
+			return "", err
+		}
+
+		responseData, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			return "", err
+		}
+
+		defer response.Body.Close()
+
+		var responseObject TranslatedDescriptionResponse
+		err = json.Unmarshal(responseData, &responseObject)
+		if err != nil {
+			return "", err
+		}
+
+		// if translation does not succeed, return original description
+		if responseObject.Success.Total == 0 {
+			return description, nil
+		}
+
+		translatedDescription = responseObject.Contents.Translated
+
+	} else {
+		response, err := http.PostForm("https://api.funtranslations.com/translate/shakespeare.json", params)
+		if err != nil {
+			return "", err
+		}
+
+		responseData, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			return "", err
+		}
+
+		defer response.Body.Close()
+
+		var responseObject TranslatedDescriptionResponse
+		err = json.Unmarshal(responseData, &responseObject)
+		if err != nil {
+			return "", err
+		}
+
+		// if translation does not succeed, return original description
+		if responseObject.Success.Total == 0 {
+			return description, nil
+		}
+
+		translatedDescription = responseObject.Contents.Translated
+	}
+	return translatedDescription, nil
 }
--- handlers/getTranslatedPokemonDescription_test.go
+++ handlers/getTranslatedPokemonDescription_test.go
@@ -1,1 +1,105 @@
 package handlers_test
+
+import (
+	"Pokedex/handlers"
+	"encoding/json"
+	"github.com/stretchr/testify/suite"
+	"io/ioutil"
+	http2 "net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type GetTranslatedPokemonDescriptionTestSuite struct {
+	suite.Suite
+	sut              *handlers.GetTranslatedPokemonDescriptionHandler
+	responseRecorder *httptest.ResponseRecorder
+	requestBuilder   *http2.Request
+}
+
+func (suite *GetTranslatedPokemonDescriptionTestSuite) TearDownTest() {
+}
+
+func (suite *GetTranslatedPokemonDescriptionTestSuite) SetupTest() {
+	suite.sut = handlers.NewGetTranslatedPokemonDescriptionHandler()
+	suite.requestBuilder = httptest.NewRequest(http2.MethodGet, "/pokemon/translated/metapod", nil)
+	suite.responseRecorder = httptest.NewRecorder()
+}
+
+func TestGetTranslatedPokemonDescriptionTestSuiteRunner(t *testing.T) {
+	suite.Run(t, new(GetTranslatedPokemonDescriptionTestSuite))
+}
+
+func (suite *GetTranslatedPokemonDescriptionTestSuite) TestReturns_StatusOk_On_Success_With_Correct_Response() {
+	// if pokemon habitat is not cave, and it is not legendary, return shakespeare translation
+	expectedName := "metapod"
+	expectedTranslatedDescription := "Coequal though 't is encased in a sturdy shell,  the corse inside is tender. 't can’t withstand a harsh attack."
+	expectedHabitat := "forest"
+	expectedLegendaryStatus := false
+
+	request := suite.requestBuilder
+
+	suite.sut.Handle(suite.responseRecorder, request)
+
+	suite.Assert().Equal(http2.StatusOK, suite.responseRecorder.Code)
+
+	body, err := ioutil.ReadAll(suite.responseRecorder.Body)
+	suite.Assert().Nil(err)
+
+	var response handlers.GetPokemonInformationResponse
+	err = json.Unmarshal(body, &response)
+	suite.Assert().Nil(err)
+
+	suite.Assert().Equal(expectedName, response.Name)
+	suite.Assert().Equal(expectedTranslatedDescription, response.Description)
+	suite.Assert().Equal(expectedHabitat, response.Habitat)
+	suite.Assert().Equal(expectedLegendaryStatus, response.IsLegendary)
+}
+
+func (suite *GetTranslatedPokemonDescriptionTestSuite) TestReturns_StatusOk_On_Success_With_Yoda_Translation_In_Response() {
+	// if pokemon habitat is cave return yoda translation
+	expectedTranslatedDescription := "Almost the same as mew’s,  its dna is.However,Vastly different,  its size and disposition are."
+
+	request := httptest.NewRequest(http2.MethodGet, "/pokemon/translated/mewtwo", nil)
+
+	suite.sut.Handle(suite.responseRecorder, request)
+	suite.Assert().Equal(http2.StatusOK, suite.responseRecorder.Code)
+
+	body, err := ioutil.ReadAll(suite.responseRecorder.Body)
+	suite.Assert().Nil(err)
+
+	var response handlers.GetPokemonInformationResponse
+	err = json.Unmarshal(body, &response)
+	suite.Assert().Nil(err)
+
+	suite.Assert().Equal(expectedTranslatedDescription, response.Description)
+}
+
+func (suite *GetTranslatedPokemonDescriptionTestSuite) TestReturns_StatusNotFound_If_Pokemon_Does_Not_Exist() {
+	request := httptest.NewRequest(http2.MethodGet, "/pokemon/fakepokemon", nil)
+
+	suite.sut.Handle(suite.responseRecorder, request)
+
+	suite.Assert().Equal(http2.StatusNotFound, suite.responseRecorder.Code)
+}
+
+func (suite *GetTranslatedPokemonDescriptionTestSuite) TestReturns_InternalServerErr_If_No_Name_Supplied_In_RequestUrl() {
+	request := httptest.NewRequest(http2.MethodGet, "/pokemon/translated/", nil)
+
+	suite.sut.Handle(suite.responseRecorder, request)
+
+	suite.Assert().Equal(http2.StatusInternalServerError, suite.responseRecorder.Code)
+}
+
+func (suite *GetTranslatedPokemonDescriptionTestSuite) TestReturns_Error_If_Decoding_Fails() {
+	request := suite.requestBuilder
+
+	suite.sut.Handle(suite.responseRecorder, request)
+	suite.Assert().Equal(http2.StatusOK, suite.responseRecorder.Code)
+
+	body := []byte(`{"name":bad_value}`)
+
+	var response handlers.GetPokemonInformationResponse
+	err := json.Unmarshal(body, &response)
+	suite.Assert().NotNil(err)
+}
